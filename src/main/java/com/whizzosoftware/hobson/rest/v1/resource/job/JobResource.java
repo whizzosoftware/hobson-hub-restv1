4636f5b4ed25d3f2975b6307dea391cae2227e69
Dan Noguerol <dan@whizzosoftware.com>
2016-11-15 14:46:44 -0700

    Added ability to stop jobs. Removed onPluginConfigurationUpdate and onDeviceConfigurationUpdate callbacks from HobsonPlugin.

--- src/main/java/com/whizzosoftware/hobson/rest/v1/resource/job/JobResource.java
+++ src/main/java/com/whizzosoftware/hobson/rest/v1/resource/job/JobResource.java
@@ -1,5 +1,6 @@
 package com.whizzosoftware.hobson.rest.v1.resource.job;
 
+import com.whizzosoftware.hobson.api.HobsonRuntimeException;
 import com.whizzosoftware.hobson.api.action.ActionManager;
 import com.whizzosoftware.hobson.api.action.job.JobInfo;
 import com.whizzosoftware.hobson.api.persist.IdProvider;
@@ -7,8 +8,10 @@ import com.whizzosoftware.hobson.rest.HobsonAuthorizer;
 import com.whizzosoftware.hobson.rest.HobsonRestContext;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.restlet.data.Status;
 import org.restlet.ext.guice.SelfInjectingServerResource;
 import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.EmptyRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ResourceException;
 
@@ -26,8 +29,23 @@ public class JobResource extends SelfInjectingServerResource {
     @Override
     protected Representation get() throws ResourceException {
         HobsonRestContext ctx = (HobsonRestContext)getRequest().getAttributes().get(HobsonAuthorizer.HUB_CONTEXT);
+        return new JsonRepresentation(getStatusJSON(actionManager.getJobInfo(ctx.getHubContext(), getAttribute("jobId"))));
+    }
 
-        JobInfo info = actionManager.getJobInfo(ctx.getHubContext(), getAttribute("jobId"));
+    @Override
+    protected Representation delete() throws ResourceException {
+        HobsonRestContext ctx = (HobsonRestContext)getRequest().getAttributes().get(HobsonAuthorizer.HUB_CONTEXT);
+
+        try {
+            actionManager.stopJob(ctx.getHubContext(), getAttribute("jobId")).sync();
+            getResponse().setStatus(Status.SUCCESS_ACCEPTED);
+            return new EmptyRepresentation();
+        } catch (InterruptedException e) {
+            throw new HobsonRuntimeException("An error occurred deleting a job", e);
+        }
+    }
+
+    private JSONObject getStatusJSON(JobInfo info) {
         JSONObject json = new JSONObject();
         json.put("status", info.getStatus().toString());
         JSONArray a = new JSONArray();
@@ -35,8 +53,6 @@ public class JobResource extends SelfInjectingServerResource {
             a.put(m);
         }
         json.put("messages", a);
-        return new JsonRepresentation(json);
+        return json;
     }
-
-
 }
