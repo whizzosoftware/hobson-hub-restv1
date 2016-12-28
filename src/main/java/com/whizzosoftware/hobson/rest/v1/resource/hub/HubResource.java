eb37129787e43366b99ae6f2b31c6a7eab8ec0fa
Dan Noguerol <dan@whizzosoftware.com>
2016-12-27 17:32:13 -0700

    Fixed issue where web console was connecting to the wrong host for websockets.

--- src/main/java/com/whizzosoftware/hobson/rest/v1/resource/hub/HubResource.java
+++ src/main/java/com/whizzosoftware/hobson/rest/v1/resource/hub/HubResource.java
@@ -1,15 +1,18 @@
-/*******************************************************************************
+/*
+ *******************************************************************************
  * Copyright (c) 2014 Whizzo Software, LLC.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ *******************************************************************************
+*/
 package com.whizzosoftware.hobson.rest.v1.resource.hub;
 
 import com.whizzosoftware.hobson.api.hub.HobsonHub;
 import com.whizzosoftware.hobson.api.hub.HubManager;
 import com.whizzosoftware.hobson.dto.ExpansionFields;
+import com.whizzosoftware.hobson.dto.context.DTOBuildContext;
 import com.whizzosoftware.hobson.dto.context.DTOBuildContextFactory;
 import com.whizzosoftware.hobson.dto.hub.HobsonHubDTO;
 import com.whizzosoftware.hobson.json.JSONAttributes;
@@ -78,9 +81,11 @@ public class HubResource extends SelfInjectingServerResource {
         ExpansionFields expansions = new ExpansionFields(getQueryValue("expand"));
         expansions.add(JSONAttributes.ITEM);
 
+        DTOBuildContext bctx = dtoBuildContextFactory.createContext(getRequest().getHostRef().getHostDomain(), ctx.getApiRoot(), expansions);
+
         HobsonHub hub = hubManager.getHub(ctx.getHubContext());
         HobsonHubDTO dto = new HobsonHubDTO.Builder(
-            dtoBuildContextFactory.createContext(ctx.getApiRoot(), expansions),
+            bctx,
             hub,
             true
         ).build();
